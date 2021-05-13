9f84a8db646c3529ff0b294e6351a9cce9c85d9f
priyankasvvv <18100btcse02719@svvindore.onmicrosoft.com>
2021-05-13 20:56:37 +0530

    final

---
+++ templates/Untitled-1.py
@@ -0,0 +1,19 @@
+a=int(input("Enter first Number"))
+
+b=int(input("Enter Second Number"))
+c=int(input("Enter third Number"))
+
+if a >b:
+    if a>c: 
+        print(a)
+    else:
+        print(c)
+
+else:
+     
+    if b>c:
+        print(b)
+    else:
+        print(c)
+
+
